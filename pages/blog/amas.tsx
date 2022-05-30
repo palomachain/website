5c52ba7dfa3327f134a17be8d6842d8dc1d7b84b
wdev733 <53801750+wdev733@users.noreply.github.com>
2022-05-30 09:15:18 -0400

    fix ui feedback (#14)

--- pages/blog/amas.tsx
+++ pages/blog/amas.tsx
@@ -5,7 +5,7 @@ import { BlogList } from "components/Blog";
 
 import { fetchBlogs, filterBlogs } from "utils/storyblok";
 
-const BlogAmas = () => {
+const BlogAmas = ({ router }) => {
   const [latestBlogs, setLatestBlog] = useState([]);
 
   useEffect(() => {
@@ -20,6 +20,17 @@ const BlogAmas = () => {
   return (
     <div className="page-container">
       <div className="blog-page-container">
+        <div
+          className="sub-nav-bar"
+          onClick={(e) => {
+            router.back();
+          }}
+        >
+          <div className="link-back-to">
+            <img src="/assets/arrows/arrow-left.png" />
+            <span>Blog</span>
+          </div>
+        </div>
         <RotatedHeader
           title="AMAS"
           rightImage="/assets/blog/ama.png"

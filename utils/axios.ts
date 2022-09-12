3de32097a678500fd84e675d143aff8c891f8d4d
wdev733 <wdev733@gmail.com>
2022-09-12 16:19:14 -0400

    add twitter api

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,12 +1,20 @@
 import axios from "axios";
 
-const axiosClient = () => {
+const axiosClient = (auth = false, token = '') => {
   const client = axios.create();
 
   client.defaults.headers.common = {
     'Content-Type': 'application/json',
     "Access-Control-Allow-Origin": "*",
   };
+
+  if (auth && token) {
+    client.defaults.headers.common = {
+      'Content-Type': 'application/json',
+      "Access-Control-Allow-Origin": "*",
+      'Authorization': `Bearer ${token}`
+    };
+  }
 
   return client;
 };
@@ -35,8 +43,9 @@ export const getPalomaTwitterWidget = async () => {
   let followers = 0;
 
   try {
-    const res = await axiosClient().get('https://cdn.syndication.twimg.com/widgets/followbutton/info.json?screen_names=paloma_chain');
+    const res = await axiosClient(true, process.env.TWITTER_API_BEARER_TOKEN).get('https://api.twitter.com/2/users/1518188063809318913?user.fields=public_metrics');
     console.log(res);
+
     if (res.status === 200) {
       const data = res.data;
 

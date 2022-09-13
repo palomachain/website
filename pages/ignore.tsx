439f598ede3e0e7158031ad8a68458136e70df9b
Jason Jacobs <nerfnerd@gmail.com>
2022-09-12 17:28:24 -0700

    remove not needed prototype

--- pages/ignore.tsx
+++ pages/ignore.tsx
@@ -1,11 +1,5 @@
 import { setCookie } from 'cookies-next';
 
-Date.prototype.addDays = function (days) {
-    let date = new Date(this.valueOf());
-    date.setDate(date.getDate() + days);
-    return date;
-}
-
 const Ignore = () => {
     setCookie('ignore', 'true', { maxAge: 60 * 60 * 24 * 7 * 10000 });
 

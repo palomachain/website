421bb468ce519bc601e9a8f1f1f77988367c55f9
Jason Jacobs <nerfnerd@gmail.com>
2022-09-14 10:35:08 -0700

    have to be verbose about adding vuepress

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ rm -rf ./paloma-docs
 git clone https://github.com/palomachain/paloma-docs.git
 cd paloma-docs/docs
 yarn install
+yarn add -D vuepress
 yarn build
 cp -r src/.vuepress/dist ../../public/docs
 cd ../../

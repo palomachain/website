9614311d0ce751466a511f6b1b733cc1845e1b05
Jason Jacobs <nerfnerd@gmail.com>
2022-09-20 23:01:28 -0700

    we no longer use a build script

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm -rf ./paloma-docs
-git clone https://github.com/palomachain/paloma-docs.git
-cd paloma-docs/docs
-yarn install
-yarn add -D vuepress
-yarn build
-cp -r src/.vuepress/dist ../../public/docs
-cd ../../

1b1c3bce7091b36177c97ed9770c329c8b97cc8b
Jason Jacobs <nerfnerd@gmail.com>
2022-10-26 14:41:00 -0700

    add defaults to counts

--- pages/index.tsx
+++ pages/index.tsx
@@ -112,13 +112,13 @@ export default function Home({ state, router }) {
             <div className="count">
               <div className="title">Total Messages</div>
               <div className="number">
-                3,633                
+                {msgs.totalMessagesCount}
               </div>
             </div>
             <div className="count">
               <div className="title">Messages per day</div>
               <div className="number">
-                122
+                {msgs.todayMessageCount}
               </div>
             </div>
           </div>

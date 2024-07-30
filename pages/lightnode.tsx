9ffe546c9b995a0b31603f56e9cd580bb2c1bdfd
jamie088 <brchwork@gmail.com>
2024-07-30 21:02:25 +0800

    add lightnode page redirect to root page

---
+++ pages/lightnode.tsx
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+const LightNode = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/");
+  });
+
+  return <></>;
+};
+
+export default LightNode;

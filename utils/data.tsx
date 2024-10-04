95e93b316c4ba5d6355ab57d0f6894dc98db23a5
jamie088 <brchwork@gmail.com>
2024-10-04 22:27:51 +0800

    fix invalid grain rewards amount

--- utils/data.tsx
+++ utils/data.tsx
@@ -1112,7 +1112,7 @@ export const PalomaInstructionsSteps: {
                     </p>
                   ),
                   copyCommand:
-                    'echo \\ "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \\ $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \\ sudo tee /etc/apt/sources.list.d/docker.list > /dev/null',
+                    'echo \\ "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \\ $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \\ \n sudo tee /etc/apt/sources.list.d/docker.list > /dev/null',
                   isTitle: false,
                 },
               ],
